54dc910a206c300c7441b5405655f4289ba9397f
RABINDRA DAKUA <dakuarabindra2001@gmail.com>
2026-02-05 18:53:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -61,3 +61,10 @@ window.addEventListener('resize', slide);
 
 
 
+  const menuToggle = document.getElementById("menuToggle");
+  const navbar = document.getElementById("navbar");
+
+  menuToggle.addEventListener("click", () => {
+      menuToggle.classList.toggle("active");
+      navbar.classList.toggle("active");
+  });
